a7edf6ccb7266c4e422351105c8a32734c686d99
billzhou200 <billzhou2005@gmail.com>
2021-09-21 16:48:54 +0800

    next snapshot 0.0.5-SNAPSHOT

--- verconfig/version.h
+++ verconfig/version.h
@@ -12,16 +12,16 @@
 //! @COMMIT_REG@ (FL_SCM_COMMIT\s+)(\S*)?(\s*,.*)
 
 // 版本号
-#define FL_VERSION 0.0.4
+#define FL_VERSION 0.0.5-SNAPSHOT
 
 #define FL_VERSION_MAJOR 0
 #define FL_VERSION_MINOR 0
-#define FL_VERSION_PATCH 4
+#define FL_VERSION_PATCH 5
 #define FL_VERSION_DESC ,
 
 // 分支名
 #define FL_SCM_BRANCH main,
 // commit_id
-#define FL_SCM_COMMIT 5c63660,
+#define FL_SCM_COMMIT 5bf7be2,
 
 #endif /* FLCONFIG_VERSION_H_ */
